01f6ae498c86ed9993af290ffd66dfa59abef237
VVoidV <lbr1994@mail.ustc.edu.cn>
2019-01-06 00:07:41 +0800

    initial commit

---
+++ src/array/Rotate.java
@@ -0,0 +1,77 @@
+package array;
+import java.util.*;
+
+/**
+ * <div class="question-detail"><div class="question-description__3U1T"><div class="translation-tool__3Ffj"><span class="" data-toggle="tooltip" data-placement="left" data-original-title="显示英文" aria-hidden="true" style="cursor: pointer;"><div class="switch-base__1Zql" data-on="false"><div class="toggle__3ZBJ"></div></div></span></div><div><p>给定一个数组，将数组中的元素向右移动&nbsp;<em>k&nbsp;</em>个位置，其中&nbsp;<em>k&nbsp;</em>是非负数。</p>
+
+<p><strong>示例 1:</strong></p>
+
+<pre><strong>输入:</strong> <code>[1,2,3,4,5,6,7]</code> 和 <em>k</em> = 3
+<strong>输出:</strong> <code>[5,6,7,1,2,3,4]</code>
+<strong>解释:</strong>
+向右旋转 1 步: <code>[7,1,2,3,4,5,6]</code>
+向右旋转 2 步: <code>[6,7,1,2,3,4,5]
+</code>向右旋转 3 步: <code>[5,6,7,1,2,3,4]</code>
+</pre>
+
+<p><strong>示例&nbsp;2:</strong></p>
+
+<pre><strong>输入:</strong> <code>[-1,-100,3,99]</code> 和 <em>k</em> = 2
+<strong>输出:</strong> [3,99,-1,-100]
+<strong>解释:</strong> 
+向右旋转 1 步: [99,-1,-100,3]
+向右旋转 2 步: [3,99,-1,-100]</pre>
+
+<p><strong>说明:</strong></p>
+
+<ul>
+	<li>尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。</li>
+	<li>要求使用空间复杂度为&nbsp;O(1) 的原地算法。</li>
+</ul>
+</div></div></div>
+ 
+ * 
+ * @author lbr19
+ *
+ */
+class RotateSolution {
+    public void rotate(int[] nums, int k) {
+    	// 可能移动的量超过数组的长度
+    	k = k % nums.length;
+    	// 输入数组的长度为 0,1时不用做任何操作， 移动的量为0时也不用操作
+    	if(nums.length < 2 || k < 1 || k == nums.length) return;
+    	
+    	// 采用数组逆序三次的办法解决
+        this.reverse(nums, 0, nums.length - 1);
+        this.reverse(nums, k, nums.length - 1);
+        this.reverse(nums, 0, k - 1);
+        
+
+    }
+    
+    public void reverse(int[] array, int begin, int end) {
+    	int temp;
+    	for(int i = 0; i <= (end - begin) / 2 ; i++) {
+    		temp = array[begin + i];
+    		array[begin + i] = array[end - i];
+    		array[end - i] = temp;
+    	}
+    }
+    
+    
+
+}
+
+public class Rotate{
+	public static void main(String[] args) {
+		int[] arr = {1,2};
+		System.out.println(Arrays.toString(arr));
+//		RotateSolution solution = new RotateSolution();
+//		solution.reverse(arr, 0, arr.length - 5);
+//		System.out.println(Arrays.toString(arr));
+		
+		RotateSolution solution = new RotateSolution();
+		solution.rotate(arr, 4);
+		System.out.println(Arrays.toString(arr));
+	}
+}
---
+++ src/array/Solution1.java
@@ -0,0 +1,30 @@
+package array;
+
+/**
+ * 
+ *  给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+
+*  不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ * @author lbr19
+ *
+ *
+
+ */
+public class Solution1 {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length < 2)
+            return nums.length;
+            
+        int current = nums[0];
+        int len=1;
+        for(int i=1; i < nums.length; i++) {
+        	if(i != len - 1 && nums[i] != current) {
+        		nums[len++] = nums[i];
+        		current = nums[i];
+        	}
+        }
+        
+        return len;
+    }
+
+}
